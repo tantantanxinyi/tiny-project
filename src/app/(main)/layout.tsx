a091e40b127883554167f34b435f57d49d9169c1
tantantanxinyi <1264030852@qq.com>
2025-02-12 23:19:33 +0800

    changes

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import { ClerkProvider } from "@clerk/nextjs";
-import { dark } from "@clerk/themes";
+import React from 'react';
+import { ClerkProvider } from '@clerk/nextjs';
+import { dark } from '@clerk/themes';
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
--- src/components/sidebar/menu-options.tsx
+++ src/components/sidebar/menu-options.tsx
@@ -1,11 +1,12 @@
 'use client';
 
-import React, { useMemo } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import {
   AgencySidebarOption,
   SubAccountSidebarOption,
   SubAccount,
 } from '@prisma/client';
+import { Sheet, SheetTrigger } from '../ui/sheet';
 
 type Props = {
   defaultOpen?: boolean;
@@ -26,11 +27,31 @@ const MenuOptions = ({
   user,
   defaultOpen,
 }: Props) => {
+  const [isMounted, setIsMounted] = useState(false); // 解决 hydration 水合问题
   const openState = useMemo(
     () => (defaultOpen ? { open: true } : {}),
     [defaultOpen],
   );
-  return <div>MenuOptions</div>;
+
+  useEffect(() => {
+    setIsMounted(true);
+  });
+
+  if (!isMounted) {
+    return null;
+  }
+  return (
+    <Sheet modal={false} {...openState}>
+      <SheetTrigger
+        asChild
+        className="absolute left-4 top-4 z-[100] md:!hidden flex"
+      >
+        <Button variant="outLine" size={'icon'}>
+          <Menu /></Menu>
+        </Button>
+      </SheetTrigger>
+    </Sheet>
+  );
 };
 
 export default MenuOptions;
