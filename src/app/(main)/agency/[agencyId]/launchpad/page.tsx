881f8b255e9de6d47792d0c41e1900a23bb12ada
tantantanxinyi <1264030852@qq.com>
2025-03-14 00:20:24 +0800

    add launchpad

---
+++ src/app/(main)/agency/[agencyId]/launchpad/page.tsx
@@ -0,0 +1,43 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import React from 'react';
+import Image from 'next/image';
+
+type Props = {};
+
+const LaunchPadPage = (props: Props) => {
+  return (
+    <div className="flex felx-col justify-center items-center">
+      <div className="w-full h-full max-w-[800px]">
+        <Card className="border-none">
+          <CardHeader>
+            <CardTitle>Lets get started</CardTitle>
+            <CardDescription>
+              Follow the steps below to get your accunt setup
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="flex flex-col gap-4">
+            <div className="flex justify-between items-center w-full border p-4 rounded-lg gap-2">
+              <div className="flex md:items-center gap-4 flex-col md:!flex-row">
+                <Image
+                  src="/appstore.png"
+                  alt="app logo"
+                  height={80}
+                  width={80}
+                  className="rounded-lg object-contain"
+                ></Image>
+              </div>
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  );
+};
+
+export default LaunchPadPage;
