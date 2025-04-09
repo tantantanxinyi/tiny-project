cae5b8ec0d9f6f0486154fd061f3c3db1b54ab20
tantantanxinyi <1264030852@qq.com>
2025-04-09 08:32:49 +0800

    add media

--- src/components/media/index.tsx
+++ src/components/media/index.tsx
@@ -9,6 +9,8 @@ import {
   CommandItem,
   CommandList,
 } from '../ui/command';
+import MediaCard from './media-card';
+import { FolderSearch } from 'lucide-react';
 
 type Props = {
   data: GetMediaFiles;
@@ -29,14 +31,29 @@ const MediaComponent = ({ data, subaccountId }: Props) => {
         />
         <CommandList className="pb-40 max-h-full">
           <CommandEmpty>No media Files</CommandEmpty>
-          <div className="flex flex-wrap gap-4 pt-4">
-            {data?.Media.map(file => (
-              <CommandItem
-                key={file.id}
-                className="p-0 max-w-[300px] w-full rounded-lg !bg-transparent !font-medium !text-white"
-              ></CommandItem>
-            ))}
-          </div>
+          <CommandGroup heading="Media Files">
+            <div className="flex flex-wrap gap-4 pt-4">
+              {data?.Media.map(file => (
+                <CommandItem
+                  key={file.id}
+                  className="p-0 max-w-[300px] w-full rounded-lg !bg-transparent !font-medium !text-white"
+                >
+                  <MediaCard file={file}></MediaCard>
+                </CommandItem>
+              ))}
+              {!data?.Media.length && (
+                <div className="flex items-center justify-center w-full flex-col">
+                  <FolderSearch
+                    size={200}
+                    className="dark:text-muted text-slate-300"
+                  />
+                  <p className="text-muted-foreground ">
+                    Empty! no files to show.
+                  </p>
+                </div>
+              )}
+            </div>
+          </CommandGroup>
         </CommandList>
       </Command>
     </div>
